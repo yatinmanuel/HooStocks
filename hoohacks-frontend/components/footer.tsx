7861b01c5f2228fa50e314faa0eda63fec1c1592
yatinmanuel <yatinmanuel@gmail.com>
2024-03-23 21:35:23 +0000

    Fixed the bug on the navigation bar

--- hoohacks-frontend/components/footer.tsx
+++ hoohacks-frontend/components/footer.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 export default function Footer() {
-    return <div className="px-4 py-6 bg-white">
+    return <div className="px-4 py-6 bg-primary text-background">
         <div className="grid justify-items-center">
-            <h1>All rights reserved HooStocks. 2024-{new Date().getFullYear()}</h1>
+            <h1>All rights reserved HooStocks. 2024-{new Date().getFullYear()}&copy;</h1>
         </div>
     </div>
 }
