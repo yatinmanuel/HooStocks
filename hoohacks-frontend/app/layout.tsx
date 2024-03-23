befaac7694a44fbe5b708ae8a7b5e294e44ecf6f
yatinmanuel <yatinmanuel@gmail.com>
2024-03-23 19:46:47 +0000

    Added Dark mode and a few extra things

--- hoohacks-frontend/app/layout.tsx
+++ hoohacks-frontend/app/layout.tsx
@@ -3,16 +3,31 @@ import "./globals.scss";
 import { GeistSans as FontSans } from "geist/font/sans";
 import { cn } from "@/lib/utils";
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+import { ThemeProvider } from "@/components/theme-provider"
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn(
         "min-h-screen bg-background font-sans antialiased", FontSans.variable
-      )}>{children}</body>
+      )}>
+            <ThemeProvider
+                attribute="class"
+                storageKey="theme"
+                defaultTheme="system"
+                disableTransitionOnChange={true}
+            >
+                {children}
+            </ThemeProvider>
+        </body>
     </html>
   );
 }
