44be427f579443ad11ac18fa50e7a37d4f5587ec
yatinmanuel <yatinmanuel@gmail.com>
2024-03-23 23:03:09 +0000

    added some spacing and some other thing to the website

--- hoohacks-frontend/app/page.tsx
+++ hoohacks-frontend/app/page.tsx
@@ -10,7 +10,6 @@ import {
     Card,
     CardTitle,
     CardFooter,
-    CardBody,
     CardHeader,
     CardContent,
     CardDescription
@@ -27,7 +26,10 @@ export default function Home() {
                 <p className="text-2xl text-[#9e9e9e] dark:text-[#dfe9e9]">stock analysis platform</p>
             </div>         
         </AuroraBorealis>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 px-6 lg:px-12 py-6 lg:py-12">
+        <div className="px-6 py-6 lg:px-12 lg:py-12">
+        <h1 className="text-4xl text-primary">Welcome to HooStocks</h1>
+        <p className="text-lg text-[#9e9e9e] dark:text-[#dfe9e9]">The future of investing is here.</p>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4 lg:mt-8">
             <Card>
                 <CardHeader>
                     <CardTitle>Card Title</CardTitle>
@@ -56,6 +58,7 @@ export default function Home() {
                 </CardContent>
             </Card>
 
+        </div>
         </div>
     </main>
   );
