393e0b3320a7ef6874e120488b5b5f0e16740e9c
yatinmanuel <yatinmanuel@gmail.com>
2024-03-23 18:57:05 +0000

    Backend?

---
+++ backend/backend/src/routing/hello-world/index.ts
@@ -0,0 +1,26 @@
+/** routing/hello-world/index.ts is a template file for each other route */
+
+import { Request, Response } from 'express';
+import Webserver from "../../index"
+
+export default {
+    // The path of the route
+    path: '/',
+    // The handlers for the route
+    GET(req: Request, res: Response, client: Webserver) {
+        console.log("Hello world")
+        res.status(200).send(`Hello, world! GET`);
+    },
+    // eslint-disable-next-line no-unused-vars
+    POST(req: Request, res: Response, client: Webserver) {
+        res.status(200).send('Hello, world! POST');
+    },
+    // eslint-disable-next-line no-unused-vars
+    PUT(req: Request, res: Response, client: Webserver) {
+        res.status(200).send('Hello, world! PUT');
+    },
+    // eslint-disable-next-line no-unused-vars
+    DELETE(req: Request, res: Response, client: Webserver) {
+        res.status(200).send('Hello, world! DELETE');
+    }
+}
