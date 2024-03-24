0240a29ead9e913a78fa8a579c5193d06d195e70
yatinmanuel <yatinmanuel@gmail.com>
2024-03-24 11:59:17 -0400

    fixed command bar

--- hoohacks-frontend/components/nav-user.tsx
+++ hoohacks-frontend/components/nav-user.tsx
@@ -58,33 +58,7 @@ export default function NavigationBar() {
     }, [])
 
 
-    function MobileSheet() {
 
-        return (
-            <Sheet>
-                <SheetTrigger>
-                    <FontAwesomeIcon icon={faBars} />
-                </SheetTrigger>
-                <SheetContent side={"top"}>
-                    <SheetTitle className="inline-flex">
-                        <Image src="/hoohacks-owl-logo.png" className="mr-2 px-2 py-2 bg-black dark:bg-transparent rounded-md" alt="Hooty-hootenson" width={45} height={40} />
-                        <span className="text-2xl font-semibold px-2 py-1">HooStocks</span>
-                    </SheetTitle>
-                    <SheetDescription>
-                        idk, its a demo. Also it aint that colourful.
-                    </SheetDescription>
-                    <ul>
-                        {/* idk what goes here */}
-                    </ul>
-                    <div className="px-4 py-4 relative">
-                        <div className="absolute bottom-5 right-5">
-                            <ModeToggle />
-                        </div>
-                    </div>
-                </SheetContent>
-            </Sheet>
-        )
-    }
 
     return (
         <nav className={(isSolid ? "shadow-gray-800 bg-neutral-200 dark:bg-neutral-900 bg-opacity-20 " : "") + "px-12 transition-all duration-300 fixed top-0 z-20 pt-2 pb-3 w-full overflow-hidden"}>
@@ -95,17 +69,18 @@ export default function NavigationBar() {
                         <h1 className="text-2xl font-semibold px-2 py-1">HooStocks</h1>
                     </Link>
                 </div>
-                <div className="inline-flex grid justify-items-center w-full">
+                <div className="inline-flex grid justify-items-center ">
 
                     {/* HOW DO I MAKE THE COMMAND BAR THE FULL SIZE OF THE PARENT DIV */}
 
-                    <Command className="grid justify-items-center w-full">
-                        <CommandInput placeholder="Search Stock Ticker" />
+                    {/* Fixed, had to remove the grid from the Command className -Yatin */}
+
+                    <Command className="justify-items-center w-full">
+                        <CommandInput placeholder="Search Stock Ticker"/>
                         <CommandList></CommandList>
                     </Command>
                 </div>
                 <div className="lg:hidden inline-flex justify-end my-2 py-1">
-                    <MobileSheet />
                 </div>
                 <div className="hidden lg:inline-flex gap-2 justify-end my-2 py-1">
                     <Avatar>
