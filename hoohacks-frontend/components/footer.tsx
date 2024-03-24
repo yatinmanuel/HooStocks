3b09ff937e9d74d5bc3c6a70d11662607e4bd9cf
yatinmanuel <yatinmanuel@gmail.com>
2024-03-23 22:58:09 -0400

    Changed footer message

--- hoohacks-frontend/components/footer.tsx
+++ hoohacks-frontend/components/footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return <div className="px-4 py-6 bg-primary text-background">
         <div className="grid justify-items-center">
-            <h1>All rights reserved HooStocks. 2024-{new Date().getFullYear()}&copy;</h1>
+            <h1>&copy; 2024-{new Date().getFullYear()} HooHacks. All Rights Reserved.</h1>
         </div>
     </div>
 }
