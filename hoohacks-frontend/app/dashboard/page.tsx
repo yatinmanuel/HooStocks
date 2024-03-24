1cff74d68321658fca0b6e8b98e05e2e9b709a57
yatinmanuel <yatinmanuel@gmail.com>
2024-03-24 14:38:37 +0000

    GoDaddy????

--- hoohacks-frontend/app/dashboard/page.tsx
+++ hoohacks-frontend/app/dashboard/page.tsx
@@ -10,53 +10,47 @@ import {
 
 
 export default function Dashboard() {
+
     return (
         <>
             <NavUser />
-            <main>
-                <div>
-                    <h1 className="pl-9 pt-20 font-bold text-xl">Dashboard</h1>
+            <main className="h-screen mt-12 px-6 py-6 lg:py-12 lg:px-12">
+                <div className="my-4">
+                    <h1 className="font-bold text-4xl ">Dashboard</h1>
                 </div>
-                <div className="pt-[1rem] grid grid-cols-3">
-                    <div className="p-3">
+                <div className="py-2 gap-4 grid grid-cols-3">
                         <Card>
                             <CardHeader>
                                 <CardTitle>
-                                    <div className="grid grid-cols-6 col-start-2 col-end-6">
+                                    <div className="flex gap-6">
                                         <p className="font-bold text-xl">Name: </p>
                                         <p className="text-xl text-left">name</p>
                                     </div>
                                 </CardTitle>
                             </CardHeader>
                         </Card>
-                    </div>
-                    <div className="p-3">
                         <Card>
                             <CardHeader>
                                 <CardTitle>
-                                    <div className="grid grid-cols-6 col-start-2 col-end-6">
+                                    <div className="flex gap-6">
                                         <p className="font-bold text-xl">Ticker: </p>
                                         <p className="text-xl text-left">xxxx</p>
                                     </div>
                                 </CardTitle>
                             </CardHeader>
                         </Card>
-                    </div>
-                    <div className="p-3">
                         <Card>
                             <CardHeader>
                                 <CardTitle>
-                                    <div className="grid grid-cols-3">
+                                    <div className="flex gap-6">
                                         <p className="font-bold text-xl">Current Price: </p>
                                         <p className="text-xl text-left">$0</p>
                                     </div>
                                 </CardTitle>
                             </CardHeader>
                         </Card>
-                    </div>
                 </div>
-                <div className="pb-[2rem] grid grid-cols-3 ">
-                    <div className="p-3">
+                <div className="pb-[2rem] grid grid-cols-3 gap-4 ">
                         <Card>
                             <CardHeader>
                                 <CardTitle className="grid justify-items-center text-xl font-bold">Letter Score</CardTitle>
@@ -66,8 +60,6 @@ export default function Dashboard() {
                                 <p className="grid justify-items-center text-7xl font-bold">A</p>
                             </CardContent>
                         </Card>
-                    </div>
-                    <div className="p-3">
                         <Card >
                             <CardHeader>
                                 <CardTitle className="grid justify-items-center text-xl font-bold">Justification</CardTitle>
@@ -77,8 +69,6 @@ export default function Dashboard() {
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             </CardContent>
                         </Card>
-                    </div>
-                    <div className="p-3">
                         <Card >
                             <CardHeader>
                                 <CardTitle className="grid justify-items-center text-xl font-bold">Index Score</CardTitle>
@@ -88,7 +78,6 @@ export default function Dashboard() {
                                 <p className="grid justify-items-center text-7xl font-bold">100</p>
                             </CardContent>
                         </Card>
-                    </div>
                 </div>
             </main>
         </>
